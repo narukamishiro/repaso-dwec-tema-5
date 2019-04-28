1cfbddd4561bc395ff12fbb1e2007656a3de9e70
isidro lucena santiago <darksirius91@gmail.com>
2019-04-28 22:03:12 +0200

    sgv animated v1

--- ws 2 (optional)/funciones.js
+++ ws 2 (optional)/funciones.js
@@ -1,10 +1,38 @@
-function move(vl){
-	var cr=document.getElementById("cir");
-	var x=cr.getAttribute("cx").value;
-	return x+vl;
+function ampliarBarra(id,nm){
+	var pt=document.getElementById(id);
+	var rec=pt.getElementsByTagName("rect");
+	var lp=rec[0].height;
+	rec[0].height=lp+nm;
 }
-function draw(nw){
-	var pr=document.getElementById("cnv");
-	var cr=document.createElement("circle");
-	cr.r=""
+function reducirBarra(id,nm){
+	var pt=document.getElementById(id);
+	var rec=pt.getElementsByTagName("rect");
+	var lp=rec[0].height;
+	rec[0].height=lp-nm;
 }
+function neodat(id){
+	var randnm;
+	var val;
+	var pt=document.getElementById(id);
+	var rec=pt.getElementsByTagName("rect");
+	var nm=rec[0].height;
+	randnm=Math.floor(Math.random()*26);
+	val=Math.floor(Math.random()*100+1);
+	if(val%2==0){
+		ampliarBarra(id,randnm);
+	}else{
+		reducirBarra(id,randnm);
+	}
+}
+function elec(){
+	var list=document.getElementsByTagName("svg");
+	var id;
+	for (x=0;x<list.length;x++){
+		id=list[x].id;
+		neodat(id);
+	}
+}
+
+setInterval(elec,5000);
+
+
